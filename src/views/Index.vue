6c02a407b6e04c03af939e5f52c7e48ffcace561
cc <2427691354@qq.com>
2020-03-23 10:06:23 +0800

    xx

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -10,12 +10,11 @@
         <!-- 右边小盒子 -->
         <div class="littleBox">
           <!-- 图标 -->
-          <div class="icon" style="background:#30d2e7">
-            <img
-              src="../assets/icon/icon1.png"
-              style="width:45%;height:1rem;padding-top: 0.3rem;"
-              alt
-            />
+          <div class="icon"
+               style="background:#30d2e7">
+            <img src="../assets/icon/icon1.png"
+                 style="width:45%;height:1rem;padding-top: 0.3rem;"
+                 alt />
           </div>
           <!-- 文字 -->
           <div class="text">
@@ -26,13 +25,16 @@
           </div>
           <!-- 箭头 -->
           <div class="arrow">
-            <img src="../assets/icon/arrow.png" alt />
+            <img src="../assets/icon/arrow.png"
+                 alt />
           </div>
         </div>
         <div class="littleBox">
           <!-- 图标 -->
-          <div class="icon" style="background:#fa6b9b">
-            <img src="../assets/icon/icon2.png" alt />
+          <div class="icon"
+               style="background:#fa6b9b">
+            <img src="../assets/icon/icon2.png"
+                 alt />
           </div>
           <!-- 文字 -->
           <div class="text">
@@ -43,13 +45,17 @@
           </div>
           <!-- 箭头 -->
           <div class="arrow">
-            <img src="../assets/icon/arrow.png" alt />
+            <img src="../assets/icon/arrow.png"
+                 alt />
           </div>
         </div>
         <div class="littleBox">
           <!-- 图标 -->
-          <div class="icon" style="background:#6b5cb7">
-            <img src="../assets/icon/icon3.png" style="width:66%;height:1rem" alt />
+          <div class="icon"
+               style="background:#6b5cb7">
+            <img src="../assets/icon/icon3.png"
+                 style="width:66%;height:1rem"
+                 alt />
           </div>
           <!-- 文字 -->
           <div class="text">
@@ -60,17 +66,17 @@
           </div>
           <!-- 箭头 -->
           <div class="arrow">
-            <img src="../assets/icon/arrow.png" alt />
+            <img src="../assets/icon/arrow.png"
+                 alt />
           </div>
         </div>
         <div class="littleBox">
           <!-- 图标 -->
-          <div class="icon" style="background:#ff9f20">
-            <img
-              src="../assets/icon/icon4.png"
-              style="width:48%;height:1rem;padding-top: 0.3rem;"
-              alt
-            />
+          <div class="icon"
+               style="background:#ff9f20">
+            <img src="../assets/icon/icon4.png"
+                 style="width:48%;height:1rem;padding-top: 0.3rem;"
+                 alt />
           </div>
           <!-- 文字 -->
           <div class="text">
@@ -81,13 +87,16 @@
           </div>
           <!-- 箭头 -->
           <div class="arrow">
-            <img src="../assets/icon/arrow.png" alt />
+            <img src="../assets/icon/arrow.png"
+                 alt />
           </div>
         </div>
         <div class="littleBox">
           <!-- 图标 -->
-          <div class="icon" style="background:#30d2e7">
-            <img src="../assets/icon/icon5.png" alt />
+          <div class="icon"
+               style="background:#30d2e7">
+            <img src="../assets/icon/icon5.png"
+                 alt />
           </div>
           <!-- 文字 -->
           <div class="text">
@@ -98,13 +107,17 @@
           </div>
           <!-- 箭头 -->
           <div class="arrow">
-            <img src="../assets/icon/arrow.png" alt />
+            <img src="../assets/icon/arrow.png"
+                 alt />
           </div>
         </div>
         <div class="littleBox">
           <!-- 图标 -->
-          <div class="icon" style="background:#fa6b9b">
-            <img src="../assets/icon/icon6.png" style="width:30%" alt />
+          <div class="icon"
+               style="background:#fa6b9b">
+            <img src="../assets/icon/icon6.png"
+                 style="width:30%"
+                 alt />
           </div>
           <!-- 文字 -->
           <div class="text">
@@ -115,7 +128,8 @@
           </div>
           <!-- 箭头 -->
           <div class="arrow">
-            <img src="../assets/icon/arrow.png" alt />
+            <img src="../assets/icon/arrow.png"
+                 alt />
           </div>
         </div>
       </div>
@@ -130,18 +144,22 @@
       <div class="table">
         <!-- 表格主体 -->
         <div class="tableBody">
-          <el-table
-            :data="tableData.slice((currentPage-1)*pagesize,currentPage*pagesize)"
-            style="width: 100%"
-            height="11.24rem"
-            row-key="id"
-            :expand-row-keys="expands"
-            @row-click="rowClick"
-          >
-            <el-table-column width="20%" type="expand">
-              <template slot-scope="props" style="padding:0">
-                <table class="detail" style="width:100%;font-size: 0.43rem;" cellspacing="0">
-                  <tr v-for="item in props.row.detail" :key="item.num" style="text-align: center;">
+          <el-table :data="tableData.slice((currentPage-1)*pagesize,currentPage*pagesize)"
+                    style="width: 100%"
+                    height="11.24rem"
+                    row-key="id"
+                    :expand-row-keys="expands"
+                    @row-click="rowClick">
+            <el-table-column width="20%"
+                             type="expand">
+              <template slot-scope="props"
+                        style="padding:0">
+                <table class="detail"
+                       style="width:100%;font-size: 0.43rem;"
+                       cellspacing="0">
+                  <tr v-for="item in props.row.detail"
+                      :key="item.num"
+                      style="text-align: center;">
                     <td style="width:1%;text-align: center;"></td>
                     <td style="width:2%;text-align: center;">{{item.num}}</td>
                     <td style="width:12%;">
@@ -172,18 +190,32 @@
                 </table>
               </template>
             </el-table-column>
-            <el-table-column label="明细" prop="det" width="30%"></el-table-column>
-            <el-table-column label="过车时间" prop="passingTime" width="150%"></el-table-column>
-            <el-table-column label="参测点" prop="referenceAddress"></el-table-column>
-            <el-table-column label="序号（车号）" prop="number"></el-table-column>
-            <el-table-column label="车型" prop="type"></el-table-column>
-            <el-table-column label="辆数" prop="trainNum"></el-table-column>
-            <el-table-column label="轴数" prop="axleNum"></el-table-column>
-            <el-table-column label="车速" prop="speed"></el-table-column>
-            <el-table-column label="环温" prop="ringTemp"></el-table-column>
-            <el-table-column label="左平均温升" prop="leftAvg"></el-table-column>
-            <el-table-column label="右平均温升" prop="rightAvg"></el-table-column>
-            <el-table-column label="警报信息" prop="alertMsg"></el-table-column>
+            <el-table-column label="明细"
+                             prop="det"
+                             width="30%"></el-table-column>
+            <el-table-column label="过车时间"
+                             prop="passingTime"
+                             width="150%"></el-table-column>
+            <el-table-column label="参测点"
+                             prop="referenceAddress"></el-table-column>
+            <el-table-column label="序号（车号）"
+                             prop="number"></el-table-column>
+            <el-table-column label="车型"
+                             prop="type"></el-table-column>
+            <el-table-column label="辆数"
+                             prop="trainNum"></el-table-column>
+            <el-table-column label="轴数"
+                             prop="axleNum"></el-table-column>
+            <el-table-column label="车速"
+                             prop="speed"></el-table-column>
+            <el-table-column label="环温"
+                             prop="ringTemp"></el-table-column>
+            <el-table-column label="左平均温升"
+                             prop="leftAvg"></el-table-column>
+            <el-table-column label="右平均温升"
+                             prop="rightAvg"></el-table-column>
+            <el-table-column label="警报信息"
+                             prop="alertMsg"></el-table-column>
           </el-table>
         </div>
         <!-- 表格分页 -->
@@ -191,13 +223,13 @@
           <div class="pageInfo">
             <span>显示第{{this.pagesize*(this.currentPage-1)+1}}到第{{this.currentPage*this.pagesize}}条记录，总共{{this.tableData.length}}条记录</span>
           </div>
-          <div class="pagination" align="right">
-            <el-pagination layout="prev, pager, next" 
-            :total="tableData.length"
-            :current-page="currentPage"
-            :page-size="pagesize"
-            @current-change="handleCurrentChange"
-            ></el-pagination>
+          <div class="pagination"
+               align="right">
+            <el-pagination layout="prev, pager, next"
+                           :total="tableData.length"
+                           :current-page="currentPage"
+                           :page-size="pagesize"
+                           @current-change="handleCurrentChange"></el-pagination>
           </div>
         </div>
       </div>
@@ -208,7 +240,7 @@
 <script>
 export default {
   name: "Index",
-  data() {
+  data () {
     return {
       //站点数
       stationNum: 8,
@@ -226,7 +258,7 @@ export default {
       //表格数据
       tableData: [
         {
-          id:1,
+          id: 1,
           //明细
           det: "序号",
           //过车时间
@@ -335,7 +367,7 @@ export default {
           ]
         },
         {
-          id:2,
+          id: 2,
           //明细
           det: "序号",
           //过车时间
@@ -444,7 +476,7 @@ export default {
           ]
         },
         {
-          id:3,
+          id: 3,
           //明细
           det: "序号",
           //过车时间
@@ -552,118 +584,9 @@ export default {
             }
           ]
         },
-       
+
         {
-          id:4,
-          //明细
-          det: "序号",
-          //过车时间
-          passingTime: "2020-02-01 18:12:13",
-          //参测点
-          referenceAddress: "田柳上行",
-          //序号
-          number: "07557",
-          //车型
-          type: "货车",
-          //辆数
-          trainNum: "56辆",
-          //轴数
-          axleNum: "230轴",
-          //车速
-          speed: "46km/h",
-          //环温
-          ringTemp: "40.3 ℃",
-          //左平均温升
-          leftAvg: "40.3℃",
-          //右平均温升
-          rightAvg: "40.3℃",
-          //警报信息
-          alertMsg: "微热",
-          detail: [
-            {
-              //序号
-              num: 1,
-              // 探测
-              probe: "下探",
-              //轴位
-              position: "0辆左4",
-              //温度
-              temp: 157.8,
-              //温升
-              tempRise: 140.3,
-              //报警
-              warning: "散热",
-              //车号
-              trainId: "C64T 4904455"
-            },
-            {
-              //序号
-              num: 2,
-              // 探测
-              probe: "下探",
-              //轴位
-              position: "0辆左4",
-              //温度
-              temp: 157.8,
-              //温升
-              tempRise: 140.3,
-              //报警
-              warning: "散热",
-              //车号
-              trainId: "C64T 4904455"
-            },
-            {
-              //序号
-              num: 3,
-              // 探测
-              probe: "下探",
-              //轴位
-              position: "0辆左4",
-              //温度
-              temp: 157.8,
-              //温升
-              tempRise: 140.3,
-              //报警
-              warning: "散热",
-              //车号
-              trainId: "C64T 4904455"
-            },
-            {
-              //序号
-              num: 4,
-              // 探测
-              probe: "下探",
-              //轴位
-              position: "0辆左4",
-              //温度
-              temp: 157.8,
-              //温升
-              tempRise: 140.3,
-              //报警
-              warning: "散热",
-              //车号
-              trainId: "C64T 4904455"
-            },
-            {
-              //序号
-              num: 5,
-              // 探测
-              probe: "下探",
-              //轴位
-              position: "0辆左4",
-              //温度
-              temp: 157.8,
-              //温升
-              tempRise: 140.3,
-              //报警
-              warning: "散热",
-              //车号
-              trainId: "C64T 4904455"
-            }
-          ]
-        },
-        {
-          id:5,
+          id: 4,
           //明细
           det: "序号",
           //过车时间
@@ -772,7 +695,7 @@ export default {
           ]
         },
         {
-          id:6,
+          id: 5,
           //明细
           det: "序号",
           //过车时间
@@ -881,7 +804,7 @@ export default {
           ]
         },
         {
-          id:7,
+          id: 6,
           //明细
           det: "序号",
           //过车时间
@@ -990,7 +913,7 @@ export default {
           ]
         },
         {
-          id:8,
+          id: 7,
           //明细
           det: "序号",
           //过车时间
@@ -1099,7 +1022,7 @@ export default {
           ]
         },
         {
-          id:9,
+          id: 8,
           //明细
           det: "序号",
           //过车时间
@@ -1208,7 +1131,7 @@ export default {
           ]
         },
         {
-          id:10,
+          id: 9,
           //明细
           det: "序号",
           //过车时间
@@ -1317,7 +1240,7 @@ export default {
           ]
         },
         {
-          id:11,
+          id: 10,
           //明细
           det: "序号",
           //过车时间
@@ -1426,7 +1349,7 @@ export default {
           ]
         },
         {
-          id:12,
+          id: 11,
           //明细
           det: "序号",
           //过车时间
@@ -1535,7 +1458,7 @@ export default {
           ]
         },
         {
-          id:13,
+          id: 12,
           //明细
           det: "序号",
           //过车时间
@@ -1644,7 +1567,7 @@ export default {
           ]
         },
         {
-          id:14,
+          id: 13,
           //明细
           det: "序号",
           //过车时间
@@ -1753,7 +1676,7 @@ export default {
           ]
         },
         {
-          id:15,
+          id: 14,
           //明细
           det: "序号",
           //过车时间
@@ -1862,7 +1785,116 @@ export default {
           ]
         },
         {
-          id:16,
+          id: 15,
+          //明细
+          det: "序号",
+          //过车时间
+          passingTime: "2020-02-01 18:12:13",
+          //参测点
+          referenceAddress: "田柳上行",
+          //序号
+          number: "07557",
+          //车型
+          type: "货车",
+          //辆数
+          trainNum: "56辆",
+          //轴数
+          axleNum: "230轴",
+          //车速
+          speed: "46km/h",
+          //环温
+          ringTemp: "40.3 ℃",
+          //左平均温升
+          leftAvg: "40.3℃",
+          //右平均温升
+          rightAvg: "40.3℃",
+          //警报信息
+          alertMsg: "微热",
+          detail: [
+            {
+              //序号
+              num: 1,
+              // 探测
+              probe: "下探",
+              //轴位
+              position: "0辆左4",
+              //温度
+              temp: 157.8,
+              //温升
+              tempRise: 140.3,
+              //报警
+              warning: "散热",
+              //车号
+              trainId: "C64T 4904455"
+            },
+            {
+              //序号
+              num: 2,
+              // 探测
+              probe: "下探",
+              //轴位
+              position: "0辆左4",
+              //温度
+              temp: 157.8,
+              //温升
+              tempRise: 140.3,
+              //报警
+              warning: "散热",
+              //车号
+              trainId: "C64T 4904455"
+            },
+            {
+              //序号
+              num: 3,
+              // 探测
+              probe: "下探",
+              //轴位
+              position: "0辆左4",
+              //温度
+              temp: 157.8,
+              //温升
+              tempRise: 140.3,
+              //报警
+              warning: "散热",
+              //车号
+              trainId: "C64T 4904455"
+            },
+            {
+              //序号
+              num: 4,
+              // 探测
+              probe: "下探",
+              //轴位
+              position: "0辆左4",
+              //温度
+              temp: 157.8,
+              //温升
+              tempRise: 140.3,
+              //报警
+              warning: "散热",
+              //车号
+              trainId: "C64T 4904455"
+            },
+            {
+              //序号
+              num: 5,
+              // 探测
+              probe: "下探",
+              //轴位
+              position: "0辆左4",
+              //温度
+              temp: 157.8,
+              //温升
+              tempRise: 140.3,
+              //报警
+              warning: "散热",
+              //车号
+              trainId: "C64T 4904455"
+            }
+          ]
+        },
+        {
+          id: 16,
           //明细
           det: "序号",
           //过车时间
@@ -1975,32 +2007,32 @@ export default {
       currentPage: 1, //初始页
       pagesize: 10, //    每页的数据
 
-      
+
       expands: []// 要展开的行，数值的元素是row的key值
     };
   },
-  methods:{
-    handleCurrentChange(currentPage){
-                this.currentPage = currentPage;
-                //console.log(this.currentPage)  //点击第几页
-        },
-     //在<table>里，我们已经设置row的key值设置为每行数据id：row-key="id"
-            rowClick(row) {
-              // console.log(row.id)
-                Array.prototype.remove = function (val) {
-                    let index = this.indexOf(val);
-                    if (index > -1) {
-                        this.splice(index, 1);
-                    }
-                };
- 
-                if (this.expands.indexOf(row.id) < 0) {
-                    this.expands.push(row.id);
-                } else {
-                    this.expands.remove(row.id);
-                }
- 
-            }
+  methods: {
+    handleCurrentChange (currentPage) {
+      this.currentPage = currentPage;
+      //console.log(this.currentPage)  //点击第几页
+    },
+    //在<table>里，我们已经设置row的key值设置为每行数据id：row-key="id"
+    rowClick (row) {
+      // console.log(row.id)
+      Array.prototype.remove = function (val) {
+        let index = this.indexOf(val);
+        if (index > -1) {
+          this.splice(index, 1);
+        }
+      };
+
+      if (this.expands.indexOf(row.id) < 0) {
+        this.expands.push(row.id);
+      } else {
+        this.expands.remove(row.id);
+      }
+
+    }
   }
 };
 </script>
@@ -2012,7 +2044,7 @@ export default {
     height: 14.5rem;
     margin: 0 1% 0 0.5%;
     float: left;
-    background: url('../assets/img/indexLine.jpg') no-repeat;
+    background: url("../assets/img/indexLine.jpg") no-repeat;
     background-size: 100% 100%;
     box-shadow: 1px 1px 4px 2px #e8e8e8;
   }
@@ -2101,7 +2133,7 @@ export default {
           background-color: #fff;
         }
       }
-     
+
       .pagination {
         width: 30%;
         height: 1.3rem;
